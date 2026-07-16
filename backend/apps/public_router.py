fe36b64994e575ffc4029bf47d2da98afb9a8d8b
Gurjot Singh <gurjotsingh855855@gmail.com>
2026-07-17 04:49:30 +0530

    minor fix item

--- backend/apps/public_router.py
+++ backend/apps/public_router.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from apps.core.admin import public_admin_site
 from apps.core.api_router import get_api_router_instance
@@ -8,4 +8,4 @@ api_router = get_api_router_instance()
 
 # api_router.register()
 
-urlpatterns = [path("admin/", public_admin_site.urls), path("api/", api_router.urls)]
+urlpatterns = [path("admin/", public_admin_site.urls, name="admin"), path("api/", include(api_router.urls))]
